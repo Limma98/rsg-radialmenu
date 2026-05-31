529b947fa3110b9eeb7896c7138c358cfa616905
RexShack <rexshackuk@gmail.com>
2026-05-31 16:12:00 +0100

    network sync walking styles

--- client/walkstyles.lua
+++ client/walkstyles.lua
@@ -8,6 +8,7 @@ AddEventHandler('walkstyles:client:normal', function()
     local ped = PlayerPedId()
     Citizen.InvokeNative(0x923583741DC87BCE, ped, 'default')
     Citizen.InvokeNative(0x89F5E7ADECCCB49C, ped, 'normal')
+    TriggerServerEvent('walkstyles:server:sync', 'default', 'normal')
 end)
 
 -- angry walkstyle
@@ -16,6 +17,7 @@ AddEventHandler('walkstyles:client:angry', function()
     local ped = PlayerPedId()
     Citizen.InvokeNative(0x923583741DC87BCE, ped, 'default')
     Citizen.InvokeNative(0x89F5E7ADECCCB49C, ped, 'angry')
+    TriggerServerEvent('walkstyles:server:sync', 'default', 'angry')
 end)
 
 -- war_veteran walkstyle
@@ -24,6 +26,7 @@ AddEventHandler('walkstyles:client:war_veteran', function()
     local ped = PlayerPedId()
     Citizen.InvokeNative(0x923583741DC87BCE, ped, 'war_veteran')
     Citizen.InvokeNative(0x89F5E7ADECCCB49C, ped, 'normal')
+    TriggerServerEvent('walkstyles:server:sync', 'war_veteran', 'normal')
 end)
 
 -- gold_panner walkstyle
@@ -32,6 +35,7 @@ AddEventHandler('walkstyles:client:gold_panner', function()
     local ped = PlayerPedId()
     Citizen.InvokeNative(0x923583741DC87BCE, ped, 'gold_panner')
     Citizen.InvokeNative(0x89F5E7ADECCCB49C, ped, 'normal')
+    TriggerServerEvent('walkstyles:server:sync', 'gold_panner', 'normal')
 end)
 
 -- lost_Man walkstyle
@@ -40,6 +44,7 @@ AddEventHandler('walkstyles:client:lost_Man', function()
     local ped = PlayerPedId()
     Citizen.InvokeNative(0x923583741DC87BCE, ped, 'lost_Man')
     Citizen.InvokeNative(0x89F5E7ADECCCB49C, ped, 'normal')
+    TriggerServerEvent('walkstyles:server:sync', 'lost_Man', 'normal')
 end)
 
 -- murfree walkstyle
@@ -48,6 +53,7 @@ AddEventHandler('walkstyles:client:murfree', function()
     local ped = PlayerPedId()
     Citizen.InvokeNative(0x923583741DC87BCE, ped, 'murfree')
     Citizen.InvokeNative(0x89F5E7ADECCCB49C, ped, 'normal')
+    TriggerServerEvent('walkstyles:server:sync', 'murfree', 'normal')
 end)
 
 -- primate walkstyle
@@ -56,4 +62,15 @@ AddEventHandler('walkstyles:client:primate', function()
     local ped = PlayerPedId()
     Citizen.InvokeNative(0x923583741DC87BCE, ped, 'primate')
     Citizen.InvokeNative(0x89F5E7ADECCCB49C, ped, 'normal')
+    TriggerServerEvent('walkstyles:server:sync', 'primate', 'normal')
+end)
+
+-- receive networked walkstyle from other players
+RegisterNetEvent('walkstyles:client:syncRemote')
+AddEventHandler('walkstyles:client:syncRemote', function(playerSrc, clipset, style)
+    local ped = GetPlayerPed(GetPlayerFromServerId(playerSrc))
+    if ped and ped ~= 0 then
+        Citizen.InvokeNative(0x923583741DC87BCE, ped, clipset)
+        Citizen.InvokeNative(0x89F5E7ADECCCB49C, ped, style)
+    end
 end)
